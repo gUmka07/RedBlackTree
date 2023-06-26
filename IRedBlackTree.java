e6167ab6b193518a365426391bb868081424706f
Dmitry Legochev <114214910+gUmka07@users.noreply.github.com>
2023-06-26 17:49:32 +0700

    Add files via upload

---
+++ IRedBlackTree.java
@@ -0,0 +1,17 @@
+public interface IRedBlackTree<T extends Comparable<T>> {
+	
+	
+    // Добавить элемент в дерево
+    
+    void add(T o);
+    
+    
+    // Удалить элемент из дерева
+   
+    boolean remove(T o);
+    
+    
+    //Возвращает true, если элемент содержится в дереве
+  
+    boolean contains(T o);
+}
